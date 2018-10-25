2a1c578bbe45ea00195ca72cc4b199adb2be9a5c
ORTI3D <e.verardo@numineo.fr>
2018-10-25 16:26:06 +0200

    EV modif lpf.2 and other (suite)

--- addin.py
+++ addin.py
@@ -170,7 +170,7 @@ class addin:
         if actionName == 'Ad_Model':
             m = self.core.dicaddin['Model']
             data = [('dimension','Choice',(m['dimension'],['2D','3D','Radial','Xsection'])),
-                    ('type','Choice',(m['type'],['confined','free'])), # EV 24/10/18 removed lpf.2 in spatial attribute 
+                    ('type','Choice',(m['type'],['confined','free'])),  
                     ('group','Choice',(m['group'],['Modflow series','Modflow UNS','Min3p','Opgeo']))#EV 18.10.18 removed 'Sutra'
                     ]
             dialg = self.dialogs.genericDialog(self.gui,'Model',data)
