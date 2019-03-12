84c8526117b41f7d79875311f42c605acafdf8cb
ORTI3D <e.verardo@numineo.fr>
2019-03-12 10:09:34 +0100

    uzf modif OA v.11/03/19

--- mtphtWriter.py
+++ mtphtWriter.py
@@ -753,7 +753,7 @@ class mtphtReader:
         if dim in ['Xsection','Radial']:
             nlay=nrow*1;nrow=1
         suff1 = 'PHT3D'
-        if opt=='Mt3dms' and 'UZT' not in  self.core.getUsedModulesList('Mt3dms'): suff1 = 'MT3D'
+        if opt=='Mt3dms' and 'UZT' not in  core.getUsedModulesList('Mt3dms'): suff1 = 'MT3D'
         if iesp<9: suff2='00'
         else : suff2='0'
         tlist = core.ttable['tlist']
