de6ff5a671d4d52d583f13d3438e9a4460d97111
muFlowRT <olivier.atteia@cegetel.net>
2024-11-08 15:51:10 +0100

    8/11/24

--- __init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = ["orti3dGui","config",
-           "modflowKeywords","mtPhtKeywords","pht3dKeywords",
-           "min3pFlowKeywords","min3pTransKeywords","min3pChemKeywords","obsKeywords",
-           "ogFlowKeywords","ogTransKeywords",
-           "modflowWriter","mtphtWriter","min3pWriter","ogWriter","sutraWriter",
-           "geometry","core","timeperiod","Pest","myInterpol","importExport",
-           "guiShow","menus","topbar",
-           "valueDialog","parameters",           
-           "qtDialogs","qtValueDialog","qtParameters","qtShow","qtVisualisation"]
